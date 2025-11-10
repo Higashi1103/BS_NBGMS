dec017a997d3f7980610588e7c933f738d190d1d
wl972242947 <wl972242947@gmail.com>
2025-11-10 14:48:08 +0900

    Update and rename Gakusei.java to GakuseiKurasu.java

--- dev/JinjiHiaringu/entity/GakuseiKurasu.java
+++ dev/JinjiHiaringu/entity/GakuseiKurasu.java
@@ -3,7 +3,7 @@ package com.example.demo.entity.hiaringu;
 import lombok.Data;
 
 @Data
-public class Gakusei {
+public class GakuseiKurasu {
 		private Integer gakuseiId;
 	    private String namae;
 	    private Integer kurasuId;
