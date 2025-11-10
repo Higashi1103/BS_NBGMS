0c45285055f7d35f45b5313475736d35969fbefb
wl972242947 <wl972242947@gmail.com>
2025-11-10 11:58:01 +0900

    Create BsNbgms2025Application.java

---
+++ dev/JinjiHiaringu/BsNbgms2025Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsNbgms2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsNbgms2025Application.class, args);
+	}
+}
