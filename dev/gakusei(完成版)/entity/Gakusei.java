5607879bf3d3ab10271a6f96dd8048cdfc1841c8
Nan-qz <nanquanzhen0410@gmail.com>
2025-11-10 11:32:35 +0900

    Create Gakusei.java

---
+++ dev/gakusei(完成版)/entity/Gakusei.java
@@ -0,0 +1,164 @@
+package com.example.demo.entity;
+
+public class Gakusei {
+
+	private Integer gakuseiId;
+
+	private Integer kurasuId;
+
+	private String namae;
+
+	private String seibetsu;
+
+	private String kagenTensu; // 分数表
+
+	private String kurasuNamae; // 班级表
+
+	private String seinengappi;
+
+	private String saishuGakureki;
+
+	private String gogakuryoku;
+
+	private Integer tensu;
+
+	private String hyouka;
+
+	private String sonota;
+
+	private String yobi1;
+
+	private String yobi2;
+
+	private String yobi3;
+
+	private Integer deleteFlag;
+
+	public Integer getGakuseiId() {
+		return gakuseiId;
+	}
+
+	public void setGakuseiId(Integer gakuseiId) {
+		this.gakuseiId = gakuseiId;
+	}
+
+	public Integer getKurasuId() {
+		return kurasuId;
+	}
+
+	public void setKurasuId(Integer kurasuId) {
+		this.kurasuId = kurasuId;
+	}
+
+	public String getKagenTensu() {
+		return kagenTensu;
+	}
+
+	public void setKagenTensu(String kagenTensu) {
+		this.kagenTensu = kagenTensu;
+	}
+
+	public String getKurasuNamae() {
+		return kurasuNamae;
+	}
+
+	public void setKurasuNamae(String kurasuNamae) {
+		this.kurasuNamae = kurasuNamae;
+	}
+
+	public String getNamae() {
+		return namae;
+	}
+
+	public void setNamae(String namae) {
+		this.namae = namae;
+	}
+
+	public String getSeibetsu() {
+		return seibetsu;
+	}
+
+	public void setSeibetsu(String seibetsu) {
+		this.seibetsu = seibetsu;
+	}
+
+	public String getSeinengappi() {
+		return seinengappi;
+	}
+
+	public void setSeinengappi(String seinengappi) {
+		this.seinengappi = seinengappi;
+	}
+
+	public String getSaishuGakureki() {
+		return saishuGakureki;
+	}
+
+	public void setSaishuGakureki(String saishuGakureki) {
+		this.saishuGakureki = saishuGakureki;
+	}
+
+	public String getGogakuryoku() {
+		return gogakuryoku;
+	}
+
+	public void setGogakuryoku(String gogakuryoku) {
+		this.gogakuryoku = gogakuryoku;
+	}
+
+	public Integer getTensu() {
+		return tensu;
+	}
+
+	public void setTensu(Integer tensu) {
+		this.tensu = tensu;
+	}
+
+	public String getHyouka() {
+		return hyouka;
+	}
+
+	public void setHyouka(String hyouka) {
+		this.hyouka = hyouka;
+	}
+
+	public String getSonota() {
+		return sonota;
+	}
+
+	public void setSonota(String sonota) {
+		this.sonota = sonota;
+	}
+
+	public String getYobi1() {
+		return yobi1;
+	}
+
+	public void setYobi1(String yobi1) {
+		this.yobi1 = yobi1;
+	}
+
+	public String getYobi2() {
+		return yobi2;
+	}
+
+	public void setYobi2(String yobi2) {
+		this.yobi2 = yobi2;
+	}
+
+	public String getYobi3() {
+		return yobi3;
+	}
+
+	public void setYobi3(String yobi3) {
+		this.yobi3 = yobi3;
+	}
+
+	public Integer getDeleteFlag() {
+		return deleteFlag;
+	}
+
+	public void setDeleteFlag(Integer deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
+}
