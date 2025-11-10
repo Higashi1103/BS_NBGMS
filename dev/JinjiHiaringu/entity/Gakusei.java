315e32450e486ea7e08a190d90728bb8ab70a414
wl972242947 <wl972242947@gmail.com>
2025-11-10 11:51:27 +0900

    Create Gakusei.java

---
+++ dev/JinjiHiaringu/entity/Gakusei.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity.hiaringu;
+
+import lombok.Data;
+
+@Data
+public class Gakusei {
+		private Integer gakuseiId;
+	    private String namae;
+	    private Integer kurasuId;
+}
