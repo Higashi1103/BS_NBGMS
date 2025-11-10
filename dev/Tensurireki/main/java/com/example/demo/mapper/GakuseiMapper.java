5abbfc44cabef4dc742e240f30abb042fa0a885f
Yougakee <yougakee@gmail.com>
2025-11-10 17:08:46 +0900

    Update GakuseiMapper.java

--- dev/Tensurireki/main/java/com/example/demo/mapper/GakuseiMapper.java
+++ dev/Tensurireki/main/java/com/example/demo/mapper/GakuseiMapper.java
@@ -10,18 +10,7 @@ import com.example.demo.entity.Gakusei;
 @Mapper
 public interface GakuseiMapper {
 	
-	//結合テスト
-	@Select("SELECT gakusei_id AS gakuseiId, namae, tensu FROM gakusei WHERE gakusei_id = #{gakuseiId}")
     Gakusei getGakuseiDetail(@Param("gakuseiId") Integer gakuseiId);
 
-    @Update("UPDATE gakusei SET tensu = #{gakusei.tensu} WHERE gakusei_id = #{gakusei.gakuseiId}")
-    int updateGakuseiInfo(@Param("gakusei") Gakusei gakusei);
-	
-	//単体テスト
-    @Select("SELECT tensu FROM gakusei WHERE gakusei_id = #{gakuseiId}")
-    Integer getCurrentScore(@Param("gakuseiId") Integer gakuseiId);
-
-    @Update("UPDATE gakusei SET tensu = #{newScore} WHERE gakusei_id = #{gakuseiId}")
-    void updateScore(@Param("gakuseiId") Integer gakuseiId, @Param("newScore") Integer newScore);
-	
+    int updateGakuseiInfo(@Param("gakusei") Gakusei gakusei);	
 }
